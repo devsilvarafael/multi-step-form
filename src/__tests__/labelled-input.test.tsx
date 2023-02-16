6678840ce7a10b5427f855e70988a4c00a930da8
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-15 23:40:49 -0300

    refactor: Add id in LabelledInput interface, to fix label htmlFor bugs and add this change in labelled-input tests.

--- src/__tests__/labelled-input.test.tsx
+++ src/__tests__/labelled-input.test.tsx
@@ -1,32 +1,30 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { PersonalInfo } from "../components/PersonalInfo/PersonalInfo";
-import userEvent from "@testing-library/user-event";
 import { LabelledInput } from "../components/LabelledInput/LabelledInput";
 
 
 describe('LabelledInput component test', function () {
     test('Should render an input', function () {
-        render(<LabelledInput name={"name"} label={"Name"} />);
+        render(<LabelledInput id={"personal-info-name"} name={"name"} label={"Name"} />);
 
         expect(screen.getByLabelText('Name')).toBeInTheDocument();
     })
 
 
     test('Should have a label', function () {
-        render(<LabelledInput label={"Testing"} name={"testing"} />);
+        render(<LabelledInput id={"personal-info-testing"} label={"Testing"} name={"testing"} />);
 
         expect(screen.getByLabelText('Testing')).toBeInTheDocument();
     })
 
     test('Should input have a valid text type', function () {
-        render(<LabelledInput type={"text"} label={"Name"} name={"name"} />);
+        render(<LabelledInput id={"personal-info-name"} type={"text"} label={"Name"} name={"name"} />);
 
         expect(screen.getByLabelText('Name')).toHaveAttribute('type', 'text')
     })
 
     test('Should input have a valid email type', function () {
-        render(<LabelledInput type={"email"} label={"Test"} name={"testing"}/>);
+        render(<LabelledInput id={"personal-info-email"} type={"email"} label={"Test"} name={"testing"}/>);
 
         expect(screen.getByLabelText('Test')).toHaveAttribute('type', 'email')
     })
--- src/components/LabelledInput/LabelledInput.tsx
+++ src/components/LabelledInput/LabelledInput.tsx
@@ -2,22 +2,23 @@ import { Container, LabelContainer, StyledInput } from "./styles";
 import { InputHTMLAttributes } from "react";
 
 interface LabelledInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    id?: string;
     label?: string;
     name?: string;
     type?: "text" | "email" | "tel";
 }
 
-export const LabelledInput = ({ label, name, type, ...props }: LabelledInputProps) => {
+export const LabelledInput = ({ id, label, name, type, ...props }: LabelledInputProps) => {
     return (
         <Container>
             <LabelContainer>
-                <label htmlFor={name}>
+                <label htmlFor={id}>
                     {label}
                 </label>
             </LabelContainer>
 
             <StyledInput
-                id={name}
+                id={id}
                 name={name}
                 type={type}
                 {...props}
